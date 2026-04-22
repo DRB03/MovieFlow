c0c7604a03b94b0e7abf6238434835353056b595
AaronCasildo <aacasildor@gmail.com>
2026-04-22 04:02:56 +0000

    feat: add global profile route

--- mymovies/urls.py
+++ mymovies/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from movies.views import index
+from users.views import profile_view
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
+    path('profile/', profile_view, name='profile'),
     path('users/', include('users.urls')),
     path('movies/', include('movies.urls')),
     path('', index, name='index')
