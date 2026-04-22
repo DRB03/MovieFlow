d06f7fcab4d9ea0159564ec77f58739d934bf8cb
AaronCasildo <aacasildor@gmail.com>
2026-04-22 04:06:38 +0000

    feat: add user register and profile routes

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     #path('', index, name='index'),
     path('login', login_view, name='login'),
+    path('register', register_view, name='register'),
     path('logout', logout_view, name='logout'),
+    path('profile', profile_view, name='users_profile'),
 ]
